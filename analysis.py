3f8d1b78f55b1e728e1be32795e80ecde6dc6ad9
Lenovo <olhaptashnik@gmail.com>
2026-04-17 17:36:32 +0100

    add average sales comment

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
+# calculate average sales
 df = pd.DataFrame(data)
 print("Продажі по містах (тимчасова версія):")
 print(df)
